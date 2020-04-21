4c3c04f2ea172c248af46b6719af4fbf53492daf
zanebliss <zanebliss@icloud.com>
2020-04-21 11:39:39 -0500

    init commit

---
+++ main.js
@@ -0,0 +1,11 @@
+const beamer = {
+    name: "Beamer",
+    species: "cat",
+    nicknames: [
+        "Fatty",
+        "Chubbs",
+        "Stupid Cat",
+        "Moldy cheeto"
+    ],
+    age: "unknown"
+}
